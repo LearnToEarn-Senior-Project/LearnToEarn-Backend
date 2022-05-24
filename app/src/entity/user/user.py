52a57707a0935ba8425940ecd8f19dd7251dda44
LnRabbiT <Decha_L@cmu.ac.th>
2022-05-25 01:56:48 +0700

    feat: done CMU OAuth and bind Google Account
    Not done get google classroom data and MS teams data

--- app/src/entity/user/user.py
+++ app/src/entity/user/user.py
@@ -1,25 +1,61 @@
 from json import dumps
 
-from bson import ObjectId
-
+import requests
 from app.src.database import DB
 
 
 class User(object):
-    def __init__(self, firstname, lastname, email, googleObject, role):
+    def __init__(self, id, firstname, lastname, email, googleObject, role):
+        self.id = id
         self.firstname = firstname
         self.lastname = lastname
         self.email = email
         self.googleObject = googleObject
         self.role = role
 
-    def addJson(self):
+    def addUserJson(self):
         return {
-            '_id': ObjectId().__str__(),
+            '_id': self.id,
             'firstname': self.firstname,
             'lastname': self.lastname,
             'email': self.email,
             'google_object': self.googleObject,
             'role': self.role
         }
-    # def bindGoogleAccount(self):
+
+    @staticmethod
+    def getAccessToken(code):
+        client_id = "DfQ7tTs1Qua9Jktfz5UupXN3uvZHsD1HUtYq617r"
+        client_secret = "RsZ34h0zGHg446tSDWQ83UJkCDsrxdjJ7CQ78U9m"
+        URI = "https://oauth.cmu.ac.th/v1/GetToken.aspx"
+        request_body = {
+            "code": code,
+            "redirect_uri": "http:/localhost:3000/redirect",
+            "client_id": client_id,
+            "client_secret": client_secret,
+            "grant_type": "authorization_code"
+        }
+        headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+        response = requests.post(URI, data=dict(request_body), headers=headers)
+        return response.json()
+
+    @staticmethod
+    def getCredentials(token):
+        URI = "https://misapi.cmu.ac.th/cmuitaccount/v1/api/cmuitaccount/basicinfo"
+        headers = {
+            "Content-Type": "application/json",
+            "Cache-Control": "no-cache",
+            "Authorization": "Bearer " + token,
+        }
+        response = requests.get(URI, headers=headers)
+        return response.json()
+
+    @staticmethod
+    def getUser(id):
+        cursor = DB.DATABASE['user'].find({"_id": id})
+        cmuUser = list(cursor)
+        json_data = dumps(cmuUser, indent=2)
+        return json_data
+
+    def addUser(self):
+        DB.insert_one(collection='user', data=self.addUserJson())
